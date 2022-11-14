b19ec7a9f93248dfda55193e3169ce1963c5852b
Kristy Gislason <kristygislason@gmail.com>
2022-11-13 20:32:41 -0800

    Add loadtweets function to make a GET request from the JSON data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -55,54 +55,46 @@ const createTweetElement = function(object) {
 const renderTweets = function(tweets) {
   const container = $('#tweets-container');
   for (let tweet in tweets) {
-    container.append(createTweetElement(tweets[tweet]));
+    container.prepend(createTweetElement(tweets[tweet]));
   }
 };
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
-
 
 /**
  * Function calls after document is ready
  */
 $(document).ready(function() {
-  renderTweets(data);
-
-  $('#new-tweet').submit(function(e) {
-    e.preventDefault();
-    let data = $(this).serialize();
-    $.post('/tweets/', data).done(function( data ) {
-      console.log("Success");
-    }).done(function( data ) {
-      console.log(data);
-      $( "#tweets-container" ).append(`<div>New tweet gos here</div>`);
+  // renderTweets(data);
+  const loadTweets = function() {
+    $.getJSON('/tweets', function( data ) {
+      console.log(data)
+      renderTweets(data);
     });
-  });  
+  };
+
+  loadTweets();
+
+  /**
+   * Post New Tweets from using AJAX
+   */
+  const postNewTweet = function() {
+    $('#new-tweet').submit(function(e) {
+      // Prevent form from submitting and reloading the page
+      e.preventDefault();
+
+      // Serialize the form data
+      let data = $(this).serialize();
+
+      // Clear the textarea
+      $(this).find('textarea').val('');
+      $.post('/tweets/', data, function(data) {        
+        console.log(data);
+        loadTweets(data);
+      }).done(function(data) {
+        console.log("Success!");
+      });
+    });
+  }
+  postNewTweet();
+
+  
 });
